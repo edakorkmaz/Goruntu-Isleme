8f381d7da666266b57083c33263b288f30452ecd
edakorkmaz <edakorkmaz8410@gmail.com>
2022-10-25 00:56:03 +0300

    .

---
+++ main.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+
+resim = cv2.imread("resim.png",0)
+cv2.imshow("manzara",resim)
+cv2.waitKey()
+
+
+
+hist=np.zeros(256) # hist dizisinin tüm elemanlarını sıfır yapar
+[w,h]=resim.shape
+for i in range(h):
+    for j in range(w):
+         renk = resim[j,i] # renk değişkeni resim matrisindeki ilgili pixelin değerini verir.
+         hist[renk]=hist[renk]+1   # pixel dğerlerinin kaç tane olduğunu bulur.
+
+a= 0   # a değişkeni  sıra ile pixel değerini artırmak için tanımladıö
+for k in range(256):
+    print(a,".pixel",hist[k])  # a pixel değerini , hist[renk] ise o pixel değerinden kaç tane olduğunu gösterir
+    a=a+1
+
+
