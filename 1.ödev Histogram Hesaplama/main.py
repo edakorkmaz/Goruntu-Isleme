405ec4607fe11c6187a20572b9e1004c0e4c0505
edakorkmaz <edakorkmaz8410@gmail.com>
2022-10-26 16:46:10 +0300

    Update main.py

--- 1.ödev Histogram Hesaplama/main.py
+++ 1.ödev Histogram Hesaplama/main.py
@@ -1,10 +1,11 @@
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
+
 
 resim = cv2.imread("resim.png",0)
 cv2.imshow("manzara",resim)
 cv2.waitKey()
-
 
 
 hist=np.zeros(256) # hist dizisinin tüm elemanlarını sıfır yapar
@@ -18,5 +19,11 @@ a= 0   # a değişkeni  sıra ile pixel değerini artırmak için tanımladıö
 for k in range(256):
     print(a,".pixel",hist[k])  # a pixel değerini , hist[renk] ise o pixel değerinden kaç tane olduğunu gösterir
     a=a+1
+
+
+plt.plot(hist)
+plt.show()
+cv2.waitKey()
+
 
 
